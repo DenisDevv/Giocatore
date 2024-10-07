a6cb77231247707fa80d19c699ec541bc6cbc30e
DenisDevv <karajedenis@gmail.com>
2024-10-07 12:40:48 +0200

    Rifatto il progetto basandolo sull'array di oggetti

--- src/TestGiocatore.java
+++ src/TestGiocatore.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
+
 public class TestGiocatore {
     public static void main(String[] args) {
-        Giocatore g1 = new Giocatore("Luigi", false, 2);
-        Giocatore g2 = new Giocatore("Mario", false, 0);
-        Giocatore g3 = new Giocatore("Masserini", false, 13);
-        System.out.println(g1.getNome() + " " + g1.getCapitano() + " " + g1.getGoals());
-        g2.setGoals(g2.getGoals() + (int) (Math.random() * 10));
-        System.out.println(g2.getNome() + " " + g2.getCapitano() + " " + g2.getGoals());
-        g3.setCapitano(true);
-        g3.setNome("Masserini il bomber");
-        System.out.println(g3.getNome() + " " + g3.getCapitano() + " " + g3.getGoals());
+        Scanner input = new Scanner(System.in);
+        System.out.print("Inserisci il numero dei giocatori nella squadra: ");
+        int n = input.nextInt();
+        Giocatore[] squadra = new Giocatore[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Inserisci il nome del giocatore: ");
+            String nome = input.next();
+            System.out.print("Il giocatore è capitano? (true/false): ");
+            Boolean capitano = input.nextBoolean();
+            System.out.print("Inserisci il numero di goal segnati: ");
+            int goals = input.nextInt();
+            squadra[i] = new Giocatore(nome, capitano, goals);
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println("Nome: " + squadra[i].getNome());
+            System.out.println("Capitano: " + squadra[i].getCapitano());
+            System.out.println("Goal segnati: " + squadra[i].getGoals());
+        }
     }
 }
