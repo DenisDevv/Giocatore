8d5c4dcb09cefbf314104f12a13ddb2e4789684f
DenisDevv <karajedenis@gmail.com>
2024-10-07 12:36:29 +0200

    Creati i metodi get/set e costruttore

--- src/Giocatore.java
+++ src/Giocatore.java
@@ -1,2 +1,32 @@
 public class Giocatore {
+    private String nome;
+    private Boolean capitano;
+    private int goals;
+    public Giocatore (String nome, Boolean capitano, int goals) {
+        this.nome = nome;
+        this.capitano = capitano;
+        this.goals = goals;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getCapitano() {
+        if (capitano) {
+            return "Capitano";
+        } else {
+            return "Non capitano";
+        }
+    }
+    public int getGoals() {
+        return goals;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCapitano(Boolean capitano) {
+        this.capitano = capitano;
+    }
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
 }
--- src/TestGiocatore.java
+++ src/TestGiocatore.java
@@ -1,6 +1,13 @@
 public class TestGiocatore {
     public static void main(String[] args) {
-        Giocatore g1 = new Giocatore("Mario", 100);
-        Giocatore g2 = new Giocatore("Luigi", 100);
+        Giocatore g1 = new Giocatore("Luigi", false, 2);
+        Giocatore g2 = new Giocatore("Mario", false, 0);
+        Giocatore g3 = new Giocatore("Masserini", false, 13);
+        System.out.println(g1.getNome() + " " + g1.getCapitano() + " " + g1.getGoals());
+        g2.setGoals(g2.getGoals() + (int) (Math.random() * 10));
+        System.out.println(g2.getNome() + " " + g2.getCapitano() + " " + g2.getGoals());
+        g3.setCapitano(true);
+        g3.setNome("Masserini il bomber");
+        System.out.println(g3.getNome() + " " + g3.getCapitano() + " " + g3.getGoals());
     }
 }
