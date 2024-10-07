d33282df5d2c5828815ea8fba7b5d5eb28a4869a
DenisDevv <karajedenis@gmail.com>
2024-10-07 12:28:34 +0200

    Creati i file

---
+++ src/TestGiocatore.java
@@ -0,0 +1,6 @@
+public class TestGiocatore {
+    public static void main(String[] args) {
+        Giocatore g1 = new Giocatore("Mario", 100);
+        Giocatore g2 = new Giocatore("Luigi", 100);
+    }
+}
